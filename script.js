53d23f1e36a5444e0a258e29a2f42d2793516127
YeetHanShan <63210086+YeetHanShan@users.noreply.github.com>
2022-01-27 21:31:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 	var space = " ";
     var speed = "150";
     var pos = 0;
-    var msg = "fuckfags.com ";
+    var msg = "Blooket ";
     function Scroll()
     {
     document.title = msg.substring(pos, msg.length) + space + msg.substring(0,pos);
