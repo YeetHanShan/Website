4277fee8e13315d854daf9a70fa73d9dee159b73
SachsTheBased <63210086+YeetHanShan@users.noreply.github.com>
2022-01-29 13:34:42 +0000

    Added Blooks

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-	var space = " ";
-    var speed = "150";
-    var pos = 0;
-    var msg = "Blooket ";
-    function Scroll()
-    {
+var space = " ";
+var speed = "150";
+var pos = 0;
+var msg = "Blooket ";
+function Scroll() {
     document.title = msg.substring(pos, msg.length) + space + msg.substring(0,pos);
     pos++;
     if (pos > msg.length) pos = 0;
     window.setTimeout("Scroll()", speed);
-    }
-    Scroll();
+  }
+Scroll();
